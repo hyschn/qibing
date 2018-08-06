a330587c5b0655ad8177452faba7e2e688952f4f
huangyongsheng <yshhuang@foxmail.com>
2018-08-06 13:58:50 +0800

    home

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MarkdownModule } from 'ngx-markdown';
 
 import { AppRoutingModule } from '../app-routing.module';
 import { HomeComponent } from './home/home.component';
@@ -7,7 +8,8 @@ import { HomeComponent } from './home/home.component';
 @NgModule({
   imports: [
     CommonModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MarkdownModule.forChild(),
   ],
   declarations: [HomeComponent],
 })
--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -7,9 +7,18 @@ import { AppService } from '../../app.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  config: JSON;
+  config = {
+    top: {
+      topic: '',
+      article: ''
+    },
+    blogs: []
+  };
   topTitle: string;
   topContent: string;
+
+  articles = [
+  ];
   constructor(private appService: AppService) { }
 
   ngOnInit() {
@@ -19,13 +28,23 @@ export class HomeComponent implements OnInit {
   getHomeConfig(): void {
     this.appService.getConfigJson().subscribe(json => {
       this.config = json.home;
-      this.topTitle = this.config['top'];
-      this.appService.getFileContext('assets/' + this.topTitle + '.md').then(
+      this.topTitle = this.config['top']['article'];
+      this.appService.getFileContext('assets/' + this.config['top']['topic'] + '/' + this.topTitle + '.md').then(
         context => {
-          this.topContent = context;
+          this.topContent = context.substring(0, 80) + '……';
         }
       );
-      this.topTitle = this.topTitle.substring(this.topTitle.lastIndexOf('/') + 1);
+      const blogs = this.config.blogs;
+      blogs.forEach(element => {
+        const article = {
+          title: element.article,
+          topic: element.topic,
+          context: '',
+          utl: 'assets/' + element.topic + '/' + element.article + '.md'
+        };
+        this.appService.getFileContext(article.utl).then(context => article.context = context.substring(0, 60) + '……');
+        this.articles.push(article);
+      });
     });
   }
 }
